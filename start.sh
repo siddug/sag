9d49d76a710609a3450c07e2e70231d59600caba
Siddhartha Gunti <siddhartha.gunti191@gmail.com>
2025-12-31 16:08:12 +0800

    nit

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "DATABASE_URL is: $DATABASE_URL"
 echo "Running prisma db push..."
-npx prisma db push --skip-generate
+npx prisma db push --url "$DATABASE_URL"
 
 echo "Starting Next.js..."
 npm run start
